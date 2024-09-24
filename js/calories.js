0964204dd4cb67a4f7b4e3568c99befc26b8cfa7
Jola Klodowska <jola.klodowska@wp.pl>
2024-09-24 20:37:03 +0200

    count calories

--- js/calories.js
+++ js/calories.js
@@ -2,14 +2,17 @@ function countCalories() {
     const weight = document.getElementById("weight");
     const height = document.getElementById("height");
     const age = document.getElementById("age");
+    const activity = document.getElementById("activity");
     const buttonCalories = document.getElementById("button__calories");
     
     buttonCalories.addEventListener('click', function () {
         const weightParam = parseFloat(weight.value);
         const heightParam = parseFloat(height.value);
         const ageParam = parseFloat(age.value);
-        const calories = '';
-        console.log(calories);
+        const activityParam = parseFloat(activity.value);
+        const PPMFemale = ((655.1 + 9.563 * weightParam + 1.85 * heightParam - 4.676 * ageParam) * activityParam).toFixed(2);
+        const PPMMale = ((66.5 + 13.75 * weightParam + 5.003 * heightParam - 6.775 * ageParam) * activityParam).toFixed(2);
+        console.log(PPMFemale);
     });
 }
 
