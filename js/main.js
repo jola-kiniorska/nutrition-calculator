f7e1624f7ad93f32822decdf638897a2fdcad0d4
Jola Klodowska <jola.klodowska@wp.pl>
2024-10-01 18:53:42 +0200

    count values

--- js/main.js
+++ js/main.js
@@ -1,65 +1,65 @@
+const bmiForm = document.querySelector('.bmi__form');
+const whrForm = document.querySelector('.whr__form');
+const caloriesForm = document.querySelector('.calories__form');
 
-const weight = document.getElementById("weight");
-const height = document.getElementById("height");
-const buttonBMI = document.getElementById("button__bmi");
-let BMI = '';
-
-if (buttonBMI) {
-    buttonBMI.addEventListener('click', function () {
-        const weightParam = parseFloat(weight.value);
-        const heightParam = parseFloat(height.value) / 100;
-        BMI = (weightParam / Math.pow(heightParam, 2)).toFixed(2);
-    });
+if (bmiForm) {
+    const buttonBMI = document.getElementById("button__bmi");
+    buttonBMI.addEventListener('click', countBMI);
 }
 
-const resultPage = document.querySelector(".wrapper.result");
-if (resultPage) {
-    const resultBMI = resultPage.querySelector('.bmi_result');
-    resultBMI.textContent = BMI;
+function countBMI() {
+    const weightParam = parseFloat(document.getElementById("weight").value);
+    const heightParam = parseFloat(document.getElementById("height").value) / 100;
+    const BMI = (weightParam / Math.pow(heightParam, 2)).toFixed(2);
+    localStorage.setItem('bmi_value', BMI);
 }
 
+const bmiResult = document.querySelector('.bmi_result');
+if (bmiResult) {
+    bmiResult.textContent = localStorage.getItem('bmi_value');
+}
 
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-
+if (whrForm) {
+    const buttonWHR = document.getElementById("button__whr");
+    buttonWHR.addEventListener('click', countWHR)
+}
 
 function countWHR() {
-    const waist = document.getElementById("waist");
-    console.log(waist)
-    const hip = document.getElementById("hip");
-    const buttonWHR = document.getElementById("button__whr");
-
-    buttonWHR.addEventListener('click', function () {
-        const waistParam = parseFloat(waist.value);
-        const hipParam = parseFloat(hip.value);
-        const WHR = (waistParam / hipParam).toFixed(2);
-        console.log(WHR);
-    });
+    const waistParam = parseFloat(document.getElementById("waist").value);
+    const hipParam = parseFloat(document.getElementById("hip").value);
+    const WHR = (waistParam / hipParam).toFixed(2);
+    localStorage.setItem('whr_value', WHR);
 }
 
-countWHR();
-
-
-
-
-
-function countCalories() {
-    const weight = document.getElementById("weight");
-    const height = document.getElementById("height");
-    const age = document.getElementById("age");
-    const activity = document.getElementById("activity");
-    const buttonCalories = document.getElementById("button__calories");
-
-    buttonCalories.addEventListener('click', function () {
-        const gender = document.querySelector('input[name="gender"]:checked').value;
-        const weightParam = parseFloat(weight.value);
-        const heightParam = parseFloat(height.value);
-        const ageParam = parseFloat(age.value);
-        const activityParam = parseFloat(activity.value);
-        const PPMFemale = ((655.1 + 9.563 * weightParam + 1.85 * heightParam - 4.676 * ageParam) * activityParam).toFixed(2);
-        const PPMMale = ((66.5 + 13.75 * weightParam + 5.003 * heightParam - 6.775 * ageParam) * activityParam).toFixed(2);
-        const CPM = gender == "female" ? PPMFemale : PPMMale;
-        console.log(CPM);
-    });
+const whrResult = document.querySelector('.whr_result');
+if (whrResult) {
+    whrResult.textContent = localStorage.getItem('whr_value');
 }
 
-countCalories();
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+if (caloriesForm) {
+    function countCalories() {
+        const weight = document.getElementById("weight");
+        const height = document.getElementById("height");
+        const age = document.getElementById("age");
+        const activity = document.getElementById("activity");
+        const buttonCalories = document.getElementById("button__calories");
+
+        buttonCalories.addEventListener('click', function () {
+            const gender = document.querySelector('input[name="gender"]:checked').value;
+            const weightParam = parseFloat(weight.value);
+            const heightParam = parseFloat(height.value);
+            const ageParam = parseFloat(age.value);
+            const activityParam = parseFloat(activity.value);
+            const PPMFemale = ((655.1 + 9.563 * weightParam + 1.85 * heightParam - 4.676 * ageParam) * activityParam).toFixed(2);
+            const PPMMale = ((66.5 + 13.75 * weightParam + 5.003 * heightParam - 6.775 * ageParam) * activityParam).toFixed(2);
+            const CPM = gender == "female" ? PPMFemale : PPMMale;
+            console.log(CPM);
+        });
+    }
+
+    countCalories();
+}
