b8f1dca79ada5fa69bf84b7e35cb7153f372e050
Jola Klodowska <jola.klodowska@wp.pl>
2024-10-02 23:11:43 +0200

    count values

--- js/main.js
+++ js/main.js
@@ -4,62 +4,76 @@ const caloriesForm = document.querySelector('.calories__form');
 
 if (bmiForm) {
     const buttonBMI = document.getElementById("button__bmi");
-    buttonBMI.addEventListener('click', countBMI);
+    buttonBMI.addEventListener('click', function () {
+        const weightParam = parseFloat(document.getElementById("weight").value);
+        const heightParam = parseFloat(document.getElementById("height").value) / 100;
+        const BMI = (weightParam / Math.pow(heightParam, 2)).toFixed(2);
+        localStorage.setItem('bmi_value', BMI);
+    });
 }
-
-function countBMI() {
-    const weightParam = parseFloat(document.getElementById("weight").value);
-    const heightParam = parseFloat(document.getElementById("height").value) / 100;
-    const BMI = (weightParam / Math.pow(heightParam, 2)).toFixed(2);
-    localStorage.setItem('bmi_value', BMI);
-}
-
-const bmiResult = document.querySelector('.bmi_result');
-if (bmiResult) {
-    bmiResult.textContent = localStorage.getItem('bmi_value');
-}
-
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 if (whrForm) {
     const buttonWHR = document.getElementById("button__whr");
-    buttonWHR.addEventListener('click', countWHR)
+    buttonWHR.addEventListener('click', function () {
+        const waistParam = parseFloat(document.getElementById("waist").value);
+        const hipParam = parseFloat(document.getElementById("hip").value);
+        const WHR = (waistParam / hipParam).toFixed(2);
+        localStorage.setItem('whr_value', WHR);
+    });
 }
-
-function countWHR() {
-    const waistParam = parseFloat(document.getElementById("waist").value);
-    const hipParam = parseFloat(document.getElementById("hip").value);
-    const WHR = (waistParam / hipParam).toFixed(2);
-    localStorage.setItem('whr_value', WHR);
-}
-
-const whrResult = document.querySelector('.whr_result');
-if (whrResult) {
-    whrResult.textContent = localStorage.getItem('whr_value');
-}
-
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 if (caloriesForm) {
-    function countCalories() {
-        const weight = document.getElementById("weight");
-        const height = document.getElementById("height");
-        const age = document.getElementById("age");
-        const activity = document.getElementById("activity");
-        const buttonCalories = document.getElementById("button__calories");
+    const buttonCalories = document.getElementById("button__calories");
+    buttonCalories.addEventListener('click', function () {
+        const gender = document.querySelector('input[name="gender"]:checked').value;
+        const weightParam = parseFloat(document.getElementById("weight").value);
+        const heightParam = parseFloat(document.getElementById("height").value);
+        const ageParam = parseFloat(document.getElementById("age").value);
+        const activityParam = parseFloat(document.getElementById("activity").value);
+        const PPMFemale = ((655.1 + 9.563 * weightParam + 1.85 * heightParam - 4.676 * ageParam) * activityParam).toFixed(2);
+        const PPMMale = ((66.5 + 13.75 * weightParam + 5.003 * heightParam - 6.775 * ageParam) * activityParam).toFixed(2);
+        const CPM = gender == "female" ? PPMFemale : PPMMale;
+        localStorage.setItem('calories_value', CPM);
+    });
+}
 
-        buttonCalories.addEventListener('click', function () {
-            const gender = document.querySelector('input[name="gender"]:checked').value;
-            const weightParam = parseFloat(weight.value);
-            const heightParam = parseFloat(height.value);
-            const ageParam = parseFloat(age.value);
-            const activityParam = parseFloat(activity.value);
-            const PPMFemale = ((655.1 + 9.563 * weightParam + 1.85 * heightParam - 4.676 * ageParam) * activityParam).toFixed(2);
-            const PPMMale = ((66.5 + 13.75 * weightParam + 5.003 * heightParam - 6.775 * ageParam) * activityParam).toFixed(2);
-            const CPM = gender == "female" ? PPMFemale : PPMMale;
-            console.log(CPM);
-        });
+const bmiResult = document.querySelector('.bmi_result');
+const whrResult = document.querySelector('.whr_result');
+const caloriesResult = document.querySelector('.calories_result');
+
+if (bmiResult) {
+    const bmiMeaning = document.querySelector('.bmi_meaning');
+    const bmiValue = localStorage.getItem('bmi_value');
+    if (bmiValue < 18.5) {
+        bmiMeaning.textContent = "underweight";
+    } else if (bmiValue >= 18.5 && bmiValue <= 24.99) {
+        bmiMeaning.textContent = "correct weight";
+    } else if (bmiValue >= 25.0) {
+        bmiMeaning.textContent = "overweight";
     }
 
-    countCalories();
+    displayResult('.bmi_result', 'bmi_value');
+}
+
+if (whrResult) {
+    const whrMeaning = document.querySelector('.whr_meaning');
+    const whrValue = localStorage.getItem('whr_value');
+    if (whrValue >= 1) {
+        whrMeaning.textContent = "apple-shaped body type";
+    } else {
+        whrMeaning.textContent = "pear-shaped body type";
+    }
+
+    displayResult('.whr_result', 'whr_value');
+}
+
+if (caloriesResult) {
+    displayResult('.calories_result', 'calories_value');
+}
+
+function displayResult(resultType, storageValue) {
+    const resultParam = document.querySelector(resultType);
+    if (resultParam) {
+        resultParam.textContent = localStorage.getItem(storageValue);
+    }
 }
